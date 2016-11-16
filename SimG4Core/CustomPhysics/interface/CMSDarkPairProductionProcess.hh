51e2b88daca144875b111eacb28fc7084619b7b1
Sushil <sushil@fnal.gov>
2016-11-16 11:47:46 +0100

    removed commented lines, disclaimer, and updated to new convention for ifndef for header

--- SimG4Core/CustomPhysics/interface/CMSDarkPairProductionProcess.hh
+++ SimG4Core/CustomPhysics/interface/CMSDarkPairProductionProcess.hh
@@ -1,4 +1,13 @@
-#ifndef CMSDarkPairProductionProcess_h
+//
+//// File name:     CMSDarkPairProductionProcess
+////
+//// Author:        Dustin Stolp (dostolp@ucdavis.edu)
+////                Sushil S. Chauhan (schauhan@cern.ch)  
+//// Creation date: 01.22.2015
+////
+//// -------------------------------------------------------------------
+
+#ifndef SimG4Core_CustomPhysics_CMSDarkPairProductionProcess_h
 #define CMSDarkPairProductionProcess_h 1
 
 #include "SimG4Core/CustomPhysics/interface/CMSDarkPairProduction.hh"
@@ -6,7 +15,6 @@
 #include "G4VEmProcess.hh"
 #include "G4Gamma.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 class G4ParticleDefinition;
 class G4VEmModel;
@@ -20,8 +28,6 @@ public:  // with description
 
   CMSDarkPairProductionProcess(G4double df = 1E0,
   		      const G4String& processName ="conv",
-                      //const G4ParticleDefinition* p = 0,
-                      //const G4double df = 1E0,
 		      G4ProcessType type = fElectromagnetic);
 
   virtual ~CMSDarkPairProductionProcess();
@@ -38,13 +44,11 @@ public:  // with description
 protected:
 
   virtual void InitialiseProcess(const G4ParticleDefinition*);
-  //double darkFactor;
 
 private:
   G4bool  isInitialised;
 };
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
   
 #endif
  
